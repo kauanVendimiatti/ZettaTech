bd22bf9a63da0af44b0abc0567af5e9d2c3366c2
kauanVendimiatti <kauan.vendimiatti@outlook.com>
2025-07-25 12:08:15 -0300

    Configura CORS para desenvolvimento frontend

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Adicionado para servir arquivos estáticos em produção
     'whitenoise.runserver_nostatic',
+    'corsheaders',
     'core',
     'rest_framework',
 ]
@@ -42,6 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     # Adicionado middleware do Whitenoise logo após o de segurança
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,3 +117,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # Endereço do seu futuro frontend local
+    "http://127.0.0.1:5173",
+]
